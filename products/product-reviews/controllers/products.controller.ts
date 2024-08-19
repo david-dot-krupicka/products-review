bd59fc0e44d8e344880d47fffb438ba19def9c9b
David Krupicka <david.krupicka@digitalwholesalesolutions.com>
2024-08-19 06:35:57 +0200

    JIRA-8: Add try catch to controllers

--- products/product-reviews/controllers/products.controller.ts
+++ products/product-reviews/controllers/products.controller.ts
@@ -5,33 +5,55 @@ import debug from 'debug';
 const log: debug.IDebugger = debug('app:products-controller');
 
 class ProductsController {
+    static readonly internalErrorMessage: string = 'Internal Server Error';
+
     async listProducts(req: Request, res: Response) {
-        const products = await productsService.list(100, 0);
-        res.status(200).send(products);
+        try {
+            const products = await productsService.list(100, 0);
+            res.status(200).send(products);
+        } catch (error) {
+            res.status(500).send(ProductsController.internalErrorMessage);
+        }
     }
 
     async getProductById(req: Request, res: Response) {
-        const product = await productsService.readById(req.body.productId);
-        log("Fetched product");
-        log(product);
-        res.status(200).send(product);
+        try {
+            const product = await productsService.readById(req.body.productId);
+            log("Fetched product");
+            log(product);
+            res.status(200).send(product);
+        } catch (error) {
+            res.status(500).send(ProductsController.internalErrorMessage);
+        }
     }
 
     async createProduct(req: Request, res: Response) {
-        const productId = await productsService.create(req.body);
-        res.status(201).send({ id: productId });
+        try {
+            const productId = await productsService.create(req.body);
+            res.status(201).send({id: productId});
+        } catch (error) {
+            res.status(500).send(ProductsController.internalErrorMessage);
+        }
     }
 
     // 204 Ref. RFC 7231: https://tools.ietf.org/html/rfc7231#section-6.3.5
     async patchProduct(req: Request, res: Response) {
-        log(await productsService.patchById(req.body.productId, req.body));
-        res.status(204).send();
+        try {
+            log(await productsService.patchById(req.body.productId, req.body));
+            res.status(204).send();
+        } catch (error) {
+            res.status(500).send(ProductsController.internalErrorMessage);
+        }
     }
 
     // 204 Ref. RFC 7231: https://tools.ietf.org/html/rfc7231#section-6.3.5
     async removeProduct(req: Request, res: Response) {
-        log(await productsService.deleteById(req.body.productId));
-        res.status(204).send();
+        try {
+            log(await productsService.deleteById(req.body.productId));
+            res.status(204).send();
+        } catch (error) {
+            res.status(500).send(ProductsController.internalErrorMessage);
+        }
     }
 }
 
--- products/product-reviews/controllers/reviews.controller.ts
+++ products/product-reviews/controllers/reviews.controller.ts
@@ -5,28 +5,46 @@ import debug from 'debug';
 const log: debug.IDebugger = debug('app:reviews-controller');
 
 class ReviewsController {
+    static readonly internalErrorMessage: string = 'Internal Server Error';
+
     async getReviewById(req: Request, res: Response) {
-        const review = await reviewsService.readById(req.body.reviewId);
-        log("Fetched review");
-        log(review);
-        res.status(200).send(review);
+        try {
+            const review = await reviewsService.readById(req.body.reviewId);
+            log("Fetched review");
+            log(review);
+            res.status(200).send(review);
+        } catch (error) {
+            res.status(500).send(ReviewsController.internalErrorMessage);
+        }
     }
 
     async createReview(req: Request, res: Response) {
-        const reviewId = await reviewsService.create(req.body);
-        res.status(201).send({ id: reviewId });
+        try {
+            const reviewId = await reviewsService.create(req.body);
+            res.status(201).send({id: reviewId});
+        } catch (error) {
+            res.status(500).send(ReviewsController.internalErrorMessage);
+        }
     }
 
     // 204 Ref. RFC 7231: https://tools.ietf.org/html/rfc7231#section-6.3.5
     async patchReview(req: Request, res: Response) {
-        log(await reviewsService.patchById(req.body.reviewId, req.body));
-        res.status(204).send();
+        try {
+            log(await reviewsService.patchById(req.body.reviewId, req.body));
+            res.status(204).send();
+        } catch (error) {
+            res.status(500).send(ReviewsController.internalErrorMessage);
+        }
     }
 
     // 204 Ref. RFC 7231: https://tools.ietf.org/html/rfc7231#section-6.3.5
     async removeReview(req: Request, res: Response) {
-        log(await reviewsService.deleteById(req.body.reviewId));
-        res.status(204).send();
+        try {
+            log(await reviewsService.deleteById(req.body.reviewId));
+            res.status(204).send();
+        } catch (error) {
+            res.status(500).send(ReviewsController.internalErrorMessage);
+        }
     }
 }
 
